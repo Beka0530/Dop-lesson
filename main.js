440777c79773e54664a11ef515867f32a2202c6a
Beka0530 <94052420+Beka0530@users.noreply.github.com>
2021-12-01 17:38:59 +0600

    create new class Animal and create object class Animal, after, console log

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello world!")
+class Animal {
+    constructor(name, height, weight) {
+        this.name = name
+        this.height = height
+        this.weight = weight
+    }
+}
+
+const dog = new Animal("Dog", 100, 30)
+
+console.log(dog)
